478c6bc1db29492edf7223dd72a83a09da48b76a
AnthonySabbagh <asabb087@uottawa.ca>
2020-01-20 16:48:50 -0500

    testing

--- dist/index.js
+++ dist/index.js
@@ -5,16 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
-const port = process.env.SERVER_PORT || 3000;
-const app = express_1.default();
-// define a route handler for the default home page
-app.get("/", (req, res) => {
-    res.send("Hello world!");
-});
-// start the Express server
-app.listen(port, () => {
-    // tslint:disable-next-line:no-console
-    console.log(`server started at http://localhost:${port}`);
-});
+const PORT = process.env.SERVER_PORT || 3000;
+express_1.default()
+    .use(express_1.default.static(path_1.default.join(__dirname, 'public')))
+    .set('views', path_1.default.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 dotenv.config();
-const port = process.env.SERVER_PORT || 3000;
-const app = express();
-
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
-
-// start the Express server
-app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+const PORT = process.env.SERVER_PORT || 3000;
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
