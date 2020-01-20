8e77c7f228478812c96b53964564a7e7d7903625
AnthonySabbagh <asabb087@uottawa.ca>
2020-01-20 15:55:00 -0500

    start of api

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+dotenv.config();
+const app = express();
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
