c6b59810c218156b3f70b3063958dfcba507a95c
AnthonySabbagh <asabb087@uottawa.ca>
2020-01-29 17:04:43 -0500

    using sequelize to connect to DB

--- src/schema.js
+++ src/schema.js
@@ -1,15 +1,6 @@
 const graphql = require("graphql");
-const pg_promise = require("pg-promise");
-const connectionString = {
-    host: "ec2-34-196-180-38.compute-1.amazonaws.com",
-    port: 5432,
-    database: "dbu0u155104t2k",
-    password: "4013f7ff7030a73b5416346a2dbf4f574b1e19f77ec6d23dcd86af6bdf35c0c3",
-    user: "cjyhlnswiregtb",
-    ssl: true
-};
-const pgp = pg_promise();
-const db = pgp(connectionString);
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('postgres://cjyhlnswiregtb:4013f7ff7030a73b5416346a2dbf4f574b1e19f77ec6d23dcd86af6bdf35c0c3@ec2-34-196-180-38.compute-1.amazonaws.com:5432/dbu0u155104t2k')
 const { 
     GraphQLObjectType,
     GraphQLID,
