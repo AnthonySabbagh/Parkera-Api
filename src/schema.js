d42b65bf4c48a9c15f3dd0ce6ab5212fd6f2969a
AnthonySabbagh <asabb087@uottawa.ca>
2020-01-30 12:32:23 -0500

    first attempt mutation

--- src/schema.js
+++ src/schema.js
@@ -2,45 +2,71 @@ const graphql = require("graphql");
 const { Sequelize } = require("sequelize");
 const DataTypes = require('sequelize/lib/data-types');
 const sequelize = new Sequelize(
-  "postgres://cjyhlnswiregtb:4013f7ff7030a73b5416346a2dbf4f574b1e19f77ec6d23dcd86af6bdf35c0c3@ec2-34-196-180-38.compute-1.amazonaws.com:5432/dbu0u155104t2k"
+    "postgres://cjyhlnswiregtb:4013f7ff7030a73b5416346a2dbf4f574b1e19f77ec6d23dcd86af6bdf35c0c3@ec2-34-196-180-38.compute-1.amazonaws.com:5432/dbu0u155104t2k"
 );
 const {
-  GraphQLObjectType,
-  GraphQLID,
-  GraphQLString,
-  GraphQLBoolean,
-  GraphQLList,
-  GraphQLSchema
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLBoolean,
+    GraphQLList,
+    GraphQLSchema
 } = require("graphql");
 const User = require("./UserModel.js")(sequelize, DataTypes);
 
 const UserType = new GraphQLObjectType({
-  name: "User",
-  fields: () => ({
-    uid: { type: GraphQLString, primaryKey:true },
-    firstname: { type: GraphQLString },
-    lastname: { type: GraphQLString },
-    user_role: { type: GraphQLString },
-    email: { type: GraphQLString },
-    phone: { type: GraphQLString }
-  })
+    name: "User",
+    fields: () => ({
+        firstname: { type: GraphQLString },
+        lastname: { type: GraphQLString },
+        user_role: { type: GraphQLString },
+        email: { type: GraphQLString, primary: true },
+        phone: { type: GraphQLString }
+    })
 });
 const RootQuery = new GraphQLObjectType({
-  name: "RootQuerytype",
-  fields: {
-    users: {
-      type: UserType,
-      resolve(parent, args) {
-        User.sync({ force: true });
-        User.findAll().then(users => {
-          console.log("All users:", JSON.stringify(users, null, 4));
-          return users;
-        });
-      }
+    name: "RootQuerytype",
+    fields: {
+        users: {
+            type: UserType,
+            resolve(parent, args) {
+                User.sync({ force: true });
+                User.findAll().then(users => {
+                    console.log("All users:", JSON.stringify(users, null, 4));
+                    return users;
+                });
+            }
+        }
     }
-  }
+});
+const mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addUser: {
+            type: UserType,
+            args: {
+                firstname: { type: GraphQLString },
+                lastname: { type: GraphQLString },
+                user_role: { type: GraphQLString },
+                email: { type: GraphQLString, primary: true },
+                phone: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                const new_user = User.create({
+                    firstname: firstname,
+                    lastname: lastname,
+                    user_role: user_role,
+                    email: email,
+                    phone: phone
+                });
+                console.log(new_user instanceof User); // true
+                console.log(new_user.firstname); // "Jane"
+            }
+        }
+    }
 });
 module.exports = new GraphQLSchema({
-  query: RootQuery
+query: RootQuery,
+    mutation
 });
 //# sourceMappingURL=schema.js.map
