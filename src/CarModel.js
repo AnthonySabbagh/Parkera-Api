eebfbce00a443330b0b8766e9ab1a5a8c706c6e7
WeihaoH <33268129+WeihaoH@users.noreply.github.com>
2020-02-11 18:22:23 -0500

    set_primarykey constraint for cid

--- src/CarModel.js
+++ src/CarModel.js
@@ -3,9 +3,14 @@ const { Sequelize, Model, DataTypes } = require("sequelize");
 ("use strict");
 module.exports = (sequelize, DataTypes) => {
     var Car_info = sequelize.define("car_info", {
+        cid:{
+            type: DataTypes.INTEGER,
+            primaryKey: true
+          },
         license: DataTypes.STRING,
         model: DataTypes.STRING,
-        color: DataTypes.STRING
+        color: DataTypes.STRING,
+        uid:DataTypes.INTEGER
     },
     {
         timestamps: false,
