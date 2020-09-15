8f513c85f54305035a24761c25d131c17b4f22e9
zaidsaeed <zaidsaeed>
2020-09-15 14:23:32 -0400

    pushing changes for tony to take a look at

--- src/ParkingSpotModel.js
+++ src/ParkingSpotModel.js
@@ -2,10 +2,11 @@ const { Sequelize, Model, DataTypes } = require("sequelize");
 
 ("use strict");
 module.exports = (sequelize, DataTypes) => {
-    var Park_spots = sequelize.define("park_spots", {
-        address: DataTypes.STRING,
-        longitude: DataTypes.DECIMAL,
-        latitude: DataTypes.DECIMAL,
-    });
-    return Park_spots;
+  var Park_spots = sequelize.define("park_spots", {
+    address: DataTypes.STRING,
+    longitude: DataTypes.DECIMAL,
+    latitude: DataTypes.DECIMAL,
+    price: DataTypes.DECIMAL,
+  });
+  return Park_spots;
 };
